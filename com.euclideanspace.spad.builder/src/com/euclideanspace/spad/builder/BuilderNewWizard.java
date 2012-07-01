6f400dd9d2e632fce5a7c7a8af837b0f5bbe95e9
Martin Baker <git87465@martinb.com>
2012-07-01 18:03:34 +0100

    started work on builder wizard

---
+++ com.euclideanspace.spad.builder/src/com/euclideanspace/spad/builder/BuilderNewWizard.java
@@ -0,0 +1,26 @@
+package com.euclideanspace.spad.builder;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class BuilderNewWizard extends Wizard implements INewWizard {
+
+	public BuilderNewWizard() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
