d9c9210b862082398f39633006238b185c026354
martinbaker <git87465@martinb.com>
2014-09-18 10:19:54 +0100

    custom lexer inserts phantom semicolon after curly brackets

--- com.euclideanspace.aldor/src/com/euclideanspace/aldor/CustomLexer.java
+++ com.euclideanspace.aldor/src/com/euclideanspace/aldor/CustomLexer.java
@@ -3,6 +3,7 @@ package com.euclideanspace.aldor;
 import java.util.Deque;
 
 import org.antlr.runtime.CharStream;
+import org.antlr.runtime.CommonToken;
 import org.antlr.runtime.RecognizerSharedState;
 import org.antlr.runtime.Token;
 
@@ -31,13 +32,26 @@ public class CustomLexer extends com.euclideanspace.aldor.parser.antlr.internal.
       tokens.addLast(token);
   } 
 
+  /*
+   * overridden to insert custom semicolon after curly brackets
+   * 
+   * (non-Javadoc)
+   * @see org.eclipse.xtext.parser.antlr.Lexer#nextToken()
+   */
   @Override
   public Token nextToken() {
       super.nextToken();
   	  //System.out.println("CustomLexer - nextToken");
       if (tokens.isEmpty())
           return Token.EOF_TOKEN;
-      return tokens.removeFirst();
+      Token firstToken = tokens.removeFirst();
+      int tt = firstToken.getType();
+      if (tt == RULE_KW_CCURLY) {
+      	  //System.out.println("CustomLexer - nextToken() "+ts +" type="+firstToken.getType());
+    	  Token semicolonToken = new CommonToken(input,RULE_KW_SEMICOLON, state.channel, state.tokenStartCharIndex, getCharIndex()-1);
+    	  emit(semicolonToken);
+      }
+      return firstToken;
   }
 
 }
--- com.euclideanspace.aldor/src/com/euclideanspace/aldor/CustomParser.java
+++ com.euclideanspace.aldor/src/com/euclideanspace/aldor/CustomParser.java
@@ -3,6 +3,18 @@ package com.euclideanspace.aldor;
 import org.antlr.runtime.CharStream;
 import org.antlr.runtime.TokenSource;
 
+/*
+ * This class is not used at present.
+ * 
+ * When I was searching for a way to customise the lexer I tried the method described
+ * in this thread:
+ * http://stackoverflow.com/questions/8039357/writing-a-custom-xtext-antlr-lexer-without-a-grammar-file
+ * This customises the EditorParser and then changes its createLexer method to call
+ * our custom lexer. 
+ * However since I am now using a simpler approach, this is no longer used.
+ * I have left this here in case I ever need to customise the parser
+ * in the future.
+ */
 public class CustomParser extends com.euclideanspace.aldor.parser.antlr.EditorParser {
   @Override
   protected TokenSource createLexer(CharStream stream) {
